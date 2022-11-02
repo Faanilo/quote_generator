4c5615c962360c01ff2338b10256c90cfe90cacf
Faanilo <faniloniainaa21@gmail.com>
2022-11-02 19:22:47 +0300

    v0.1.1

--- quote/src/App.js
+++ quote/src/App.js
@@ -3,23 +3,25 @@ import Loading from "../src/components/Loading";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "../src/assets/App.css";
-import Navbar from "../src/components/Navbar"
+import Navbar from "../src/components/Navbar";
 function App() {
   const [data, setData] = useState([]);
   let [loading, setIsLoading] = useState(true);
-  /*const getData = () => {
-    fetch("https://api.adviceslip.com/advice")
-      .then((res) => {
-        console.log(res);
-        setTimeout(() => {
-          setData(res.data);
-          setIsLoading(false);
-        }, 3000);
-        console.log(data);
-      })
-      .catch((err) => console.log(err));
-  };*/
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const options = {
+      method: "GET",
+      url: "https://famous-quotes4.p.rapidapi.com/random",
+      params: { category: "all", count: "1" },
+      headers: {
+        "X-RapidAPI-Key": "1cf95f6a55msh2a63d3ac8a4b884p14cc72jsn2534e7f20c1c",
+        "X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
+      },
+    };
+    const res = await axios.request(options).then((res) => {
+      setData(res.data);
+    });
+  };
   useEffect(() => {
     const options = {
       method: "GET",
@@ -46,7 +48,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-    <Navbar/>
+      <Navbar />
       {loading ? (
         <Loading />
       ) : (
@@ -58,14 +60,20 @@ function App() {
                   <div>
                     <h5 className="card-text" key={item.id}>
                       "{item.text}"
-                     
                     </h5>
                     <p className="card-text"> ~{item.author}~ </p>
                   </div>
                 );
               })}
               <center>
-              <br/> <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Get Motivation</button>
+                <br />{" "}
+                <button
+                  type="button"
+                  className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                  onClick={handleSubmit}
+                >
+                  Get Motivation
+                </button>
               </center>
             </div>
           </div>
